d49f903d5a4fc70fba27eb63bde7705b4a521249
kwstaki <zoukas1sb4ck@hotmail.com>
2018-01-23 18:32:43 +0200

    Second PageNew

---
+++ target/generated-sources/annotations/com/thesis/tuc/services/rest/responseDTOs/QFlight.java
@@ -0,0 +1,55 @@
+package com.thesis.tuc.services.rest.responseDTOs;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QFlight is a Querydsl query type for Flight
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QFlight extends EntityPathBase<Flight> {
+
+    private static final long serialVersionUID = -1399983746L;
+
+    public static final QFlight flight = new QFlight("flight");
+
+    public final StringPath fromArrivalTime = createString("fromArrivalTime");
+
+    public final StringPath fromCompany = createString("fromCompany");
+
+    public final StringPath fromDate = createString("fromDate");
+
+    public final StringPath fromDepartureTime = createString("fromDepartureTime");
+
+    public final StringPath id = createString("id");
+
+    public final StringPath price = createString("price");
+
+    public final StringPath toArrivalTime = createString("toArrivalTime");
+
+    public final StringPath toCompany = createString("toCompany");
+
+    public final StringPath toDate = createString("toDate");
+
+    public final StringPath toDepartureTime = createString("toDepartureTime");
+
+    public QFlight(String variable) {
+        super(Flight.class, forVariable(variable));
+    }
+
+    public QFlight(Path<? extends Flight> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QFlight(PathMetadata metadata) {
+        super(Flight.class, metadata);
+    }
+
+}
+
